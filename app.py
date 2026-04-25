449fa1e0254f3e2a46bd9783f1638575e3fd5e97
dewiuuuu <nugrainidewi@gmail.com>
2026-04-25 10:51:21 +0700

    tambah fitur form pengajuan

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from controllers.auth_controller import auth
 from config import Config
+from controllers.form_controller import form_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 app.register_blueprint(auth)
+app.register_blueprint(form_bp, url_prefix='/form')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ controllers/form_controller.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, render_template, request, redirect, session
+from utils.db import get_db_connection
+
+form_bp = Blueprint('form', __name__)
+
+@form_bp.route('/', methods=['GET', 'POST'])
+def form_pengajuan():
+    
+    if 'user' not in session:
+        return redirect('/')
+
+    if request.method == 'POST':
+        no_of_dependents = request.form['no_of_dependents']
+        self_employed = request.form['self_employed']
+        income_annum = request.form['income_annum']
+        loan_amount = request.form['loan_amount']
+        loan_term = request.form['loan_term']
+        cibil_score = request.form['cibil_score']
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        query = """
+        INSERT INTO pengajuan 
+        (id_user, no_of_dependents, self_employed, income_annum, loan_amount, loan_term, cibil_score)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+
+        cursor.execute(query, (
+            session['user'],
+            no_of_dependents,
+            self_employed,
+            income_annum,
+            loan_amount,
+            loan_term,
+            cibil_score
+        ))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return redirect('/dashboard')
+
+    return render_template('form_pengajuan.html')
